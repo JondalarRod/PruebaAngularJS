091e12d89e98db92aefc71e8b8ebfee3eb48f74f
JondalarRod <rodriguezjondalar@gmail.com>
2021-09-19 18:33:09 -0500

    agregando servicio sencillo

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,25 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', '$sceDelegateProvider', function($routeProvider,$sceDelegateProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
+  $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain. **.
+    'https://localhost:3000/**'
+  ]);
 }])
 
-.controller('View1Ctrl', [function() {
 
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+  //https://www.geoplugin.net/json.gp?jsoncallback=JSON_CALLBACK
+  $scope.geo =  {}
+  $http.jsonp('https://localhost:3000/cuentas')
+  .then(function(respuesta){
+    $scope.geo =  respuesta
+  })
 }]);
